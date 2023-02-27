de07dcd73d952797c37f13f8201c78065c1f8d41
rioil <rioil.dev@gmail.com>
2023-02-27 11:36:05 +0900

    トレイアイコンを追加

--- AvaloniaTrial/App.axaml.cs
+++ AvaloniaTrial/App.axaml.cs
@@ -1,13 +1,22 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AvaloniaTrial.ViewModels;
 using AvaloniaTrial.Views;
+using Reactive.Bindings;
+using System;
 
 namespace AvaloniaTrial
 {
     public partial class App : Application
     {
+        public App()
+        {
+            ExitCommand = new ReactiveCommandSlim().WithSubscribe(Shutdown);
+            OpenCommand = new ReactiveCommandSlim().WithSubscribe(Open);
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -17,13 +26,58 @@ namespace AvaloniaTrial
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                desktop.MainWindow = CreateMainWindow();
+                desktop.ShutdownMode = ShutdownMode.OnExplicitShutdown;
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void MainWindow_Closed(object? sender, EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow.Closed -= MainWindow_Closed;
+                desktop.MainWindow = null;
+            }
+        }
+
+        private void Shutdown()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
+        public ReactiveCommandSlim ExitCommand { get; }
+
+        private void Open()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                if (desktop.MainWindow is null)
+                {
+                    desktop.MainWindow = CreateMainWindow();
+                    desktop.MainWindow.Show();
+                }
+                else
+                {
+                    desktop.MainWindow.WindowState = WindowState.Normal;
+                    desktop.MainWindow.Activate();
+                }
+            }
+        }
+        public ReactiveCommandSlim OpenCommand { get; }
+
+        private MainWindow CreateMainWindow()
+        {
+            var window = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+            window.Closed += MainWindow_Closed;
+            
+            return window;
         }
     }
 }
